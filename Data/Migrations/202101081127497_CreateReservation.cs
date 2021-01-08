bcf545a35bf7850584e3cdac724cb1de16c90da0
MariyanKarakiev <77015673+MariyanKarakiev@users.noreply.github.com>
2021-01-08 13:41:10 +0200

    Added migrations

---
+++ Data/Migrations/202101081127497_CreateReservation.cs
@@ -0,0 +1,32 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateReservation : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Reservations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NameofReservation = c.String(),
+                        Age = c.String(),
+                        RoomNumber = c.Int(nullable: false),
+                        RoomType = c.String(),
+                        Smoke = c.Boolean(nullable: false),
+                        CalculatedPrice = c.Double(nullable: false),
+                        PeriodOfReservation = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Reservations");
+        }
+    }
+}
---
+++ Data/ReservationContext.cs
@@ -0,0 +1,20 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ReservationDbContext : DbContext
+    {
+        public ReservationDbContext()
+            : base("name=ReservationDb")
+        {
+
+        }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
